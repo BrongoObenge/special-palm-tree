8c75ac862f14db79f4c4af9ca27322313dfc3b23
Jiar <e@mail.email>
2016-03-29 09:01:33 +0200

    Added files needed for the project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # special-palm-tree
 palm trees are the best thing that has happened to humankind
+# Req
+- [ ]
+- [ ]
+- [ ]
+- [ ]
+- [ ]
--- src/nl/specialtree/core/Main.scala
+++ src/nl/specialtree/core/Main.scala
@@ -1,8 +1,17 @@
 package nl.specialtree.core
 
+import scala.io
 /**
   * Created by razmaklat on 22-3-16.
   */
+class Printer {
+  def askInput():String = {
+    scala.io.StdIn.readLine()
+  }
+}
+
 object Main extends App{
-    println("Hello Trees")
+  val a = new Printer
+  a askInput()
+
 }
