a214c9ef2d97b0f0dd71451faaae74106b33b855
Olson Yarzagaray <olsonyarzagaray@outlook.com>
2016-03-29 09:57:24 +0200

    basic import

--- src/nl/specialtree/core/Main.scala
+++ src/nl/specialtree/core/Main.scala
@@ -1,17 +1,23 @@
 package nl.specialtree.core
 
+import nl.specialtree.config.Config
+
 import scala.io
+import scala.io.Source
 /**
   * Created by razmaklat on 22-3-16.
   */
-class Printer {
-  def askInput():String = {
-    scala.io.StdIn.readLine()
-  }
-}
-
 object Main extends App{
-    println("Hello Trees")
-    val x = "hello tree"
-    println(x)
+  var items:Map[String,Map[Int,Double]] = Map()
+  val source = Source.fromFile(Config.dataLocation)
+  try {
+    for(line <- source.getLines()) {
+      val ln = line.split(',')
+      println(s"user ${ln(0)} item ${ln(1)} itemRating ${ln(2)}")
+    }
+  } finally {
+    source.close()
+  }
+
+
 }
